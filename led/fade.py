30bfa3adbdfb314133762515be9bc638cc1272ce
yk <yosizuka1@gmail.com>
2019-03-19 17:16:24 +0900

    add fade

---
+++ led/fade.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+led_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+
+pwm_led = GPIO.PWM(led_pin, 50)
+pwm_led.start(0)
+
+try:
+    while (True):
+        for dc in range(0, 100, 5):
+            pwm_led.ChangeDutyCycle(dc)
+            time.sleep(0.05)
+        for dc in range(0, 100, -5):
+            pwm_led.ChangeDutyCycle(dc)
+            time.sleep(0.05)
+finally:
+    print("Cleaning Up!")
+    GPIO.cleanup()
