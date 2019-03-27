92f8b69df537910c8347ff3842573b4279b1a22f
yk <yosizuka1@gmail.com>
2019-03-27 16:37:08 +0900

    add cleanup

--- motor/motor.py
+++ motor/motor.py
@@ -7,9 +7,13 @@ motor_pin = 18
 pi.wiringPiSetupGpio()
 pi.pinMode( motor_pin, pi.OUTPUT)
 
-while True:
-    pi.digitalWrite(motor_pin, pi.HIGH)
-    time.sleep(2)
+try:
+    while True:
+        pi.digitalWrite(motor_pin, pi.HIGH)
+        time.sleep(2)
 
-    pi.digitalWrite(motor_pin, pi.LOW)
-    time.sleep(2)    
+        pi.digitalWrite(motor_pin, pi.LOW)
+        time.sleep(2)
+finally:
+    print("Cleaning Up!")
+    pi.cleanup()
